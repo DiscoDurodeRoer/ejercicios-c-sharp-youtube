d55b740142b11b0d4f7fea24d749f8e6efc63576
DiscoDurodeRoer <30374642+DiscoDurodeRoer@users.noreply.github.com>
2022-06-04 10:47:27 +0200

    c# 1-2 ficheros

---
+++ ficheros/ficheros 01/ejercicio 1/Ficheros1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ficheros1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                StreamReader sr = new StreamReader("ejemplo.txt");
+
+                string linea;
+
+                while ((linea = sr.ReadLine()) != null)
+                {
+                    System.Console.WriteLine(linea);
+                }
+
+                sr.Close();
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("No se ha encontrado el archivo");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ficheros/ficheros 02/ejercicio 2/Ficheros2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ficheros2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+                StreamWriter sw = new StreamWriter("ejemplo.txt");
+
+                string[] lineas = {
+                    "Hola amigos",
+                    "Bienvenidos a un nuevo video de ficheros",
+                    "Si te gusto el video, dale a like y subscribete"
+                };
+
+                foreach (string linea in lineas)
+                {
+                    sw.WriteLine(linea);
+                }
+
+                sw.Close();
+
+                Console.WriteLine("Fichero escrito");
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
