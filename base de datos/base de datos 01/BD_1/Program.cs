c4026bdc2b832b83695c1f0a54e315bb5d8875e0
DiscoDurodeRoer <30374642+DiscoDurodeRoer@users.noreply.github.com>
2022-06-24 16:34:24 +0200

    base de datos C#

---
+++ base de datos/base de datos 01/BD_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BD_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            OleDbConnection conexion = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0; Data Source=|DataDirectory|Database.accdb");
+
+            conexion.Open();
+
+            String sql = "SELECT * FROM clientes";
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter(sql, conexion);
+
+            DataSet d = new DataSet();
+            adapter.Fill(d);
+
+            foreach (DataRow row in d.Tables[0].Rows)
+            {
+                Console.WriteLine("ID: " + row["ID"]);
+                Console.WriteLine("nombre: " + row["nombre"]);
+                Console.WriteLine("apellidos: " + row["apellidos"]);
+                Console.WriteLine("edad: " + row["edad"]);
+                Console.WriteLine("");
+            }
+
+
+            conexion.Close();
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ base de datos/base de datos 02/BD_2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BD_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            OleDbConnection conexion = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0; Data Source=|DataDirectory|Database.accdb");
+
+            conexion.Open();
+
+            Console.WriteLine("Escribe el nombre");
+            String nombre = Console.ReadLine();
+
+            Console.WriteLine("Escribe los apellidos");
+            String apellidos = Console.ReadLine();
+
+            Console.WriteLine("Escribe la edad");
+            int edad = Convert.ToInt32(Console.ReadLine());
+
+            String sql = "INSERT INTO clientes (nombre, apellidos, edad) VALUES(";
+            sql += "'"+nombre+"', ";
+            sql += "'" + apellidos + "', ";
+            sql += edad + ");";
+
+            OleDbCommand cmd = new OleDbCommand();
+            cmd.Connection = conexion;
+            cmd.CommandText = @sql;
+
+            if (cmd.ExecuteNonQuery() > 0)
+            {
+                Console.WriteLine("Se ha insertado los datos correctamente");
+            }
+            else
+            {
+                Console.WriteLine("No se ha insertado los datos correctamente");
+            }
+
+
+            conexion.Close();
+
+            Console.ReadLine();
+
+        }
+    }
+}
